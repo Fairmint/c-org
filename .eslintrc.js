6e7febc880248b841c601ece8bf07656fc1b3f01
Hardly Difficult <hardlydiff@gmail.com>
2019-06-05 14:40:31 -0700

    +eslint / prettier (#11)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    artifacts: 'readonly',
+    contract: 'readonly',
+    it: 'readonly',
+    assert: 'readonly',
+    before: 'readonly',
+    describe: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+};
