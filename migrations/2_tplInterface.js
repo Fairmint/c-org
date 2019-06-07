fde62a5e2e69c9c35dd7a9eea63de4fd9a3ba327
Hardly Difficult <hardlydiff@gmail.com>
2019-06-06 19:25:22 -0700

    Rename

--- migrations/2_tplInterface.js
+++ migrations/2_tplInterface.js
@@ -1,6 +1,6 @@
 const tplInterface = artifacts.require('TPLInterface-AutoApprove');
 
-module.exports = function deployCorg(deployer) {
+module.exports = function deployTplInterface(deployer) {
   // TODO switch to real TPL interface
   deployer.deploy(tplInterface);
 };
