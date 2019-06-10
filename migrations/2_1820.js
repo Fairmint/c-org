4f69bf34198305f4f885823afc02d4a28f47b052
hardlydifficult <hardlydiff@gmail.com>
2019-06-10 16:09:04 -0700

    lint

--- migrations/2_1820.js
+++ migrations/2_1820.js
@@ -1,6 +1,7 @@
 const erc1820 = require('erc1820');
 
-module.exports = function deploy1820() {
+// eslint-disable-next-line no-unused-vars
+module.exports = function deploy1820(deployer) {
   // TODO skip if network != local
   erc1820.deploy(web3);
 };
