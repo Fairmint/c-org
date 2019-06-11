c0d6f82d0e8386b6fc9853a052a95d1e8bab229e
hardlydifficult <hardlydiff@gmail.com>
2019-06-10 18:32:03 -0700

    Comment update

--- contracts/IAuthorization.sol
+++ contracts/IAuthorization.sol
@@ -1,37 +1,38 @@
 pragma solidity ^0.5.0;
 
-/**
-  * Check if a transfer should be allowed to proceed.
-  * @param _operator address The account initiating this transfer, may be the same as `_from`
-  * @param _from address The account holding the tokens to be sent and initiating this transfer.
-  * @param _to address The account of the recipient. When selling tokens _to will be the ZERO_ADDRESS.
-  * @param _value uint256 The amount to transfer.
-  */
 interface IAuthorization {
   /**
    * Reverts if the transfer is not approved.
+   * @param _operator address The account initiating this transfer, may be the same as `_from`
+   * @param _from address The account holding the tokens to be sent and initiating this transfer.
+   * @param _to address The account of the recipient. When selling tokens _to will be the ZERO_ADDRESS.
+   * @param _amount uint256 The amount to transfer.
    */
   function authorizeTransfer(
     address _operator,
     address _from,
     address _to,
-    uint256 _value
+    uint256 _amount
   ) external;
 
   /**
+   * @param _operator address The account initiating this transfer, may be the same as `_from`
+   * @param _from address The account holding the tokens to be sent and initiating this transfer.
+   * @param _to address The account of the recipient. When selling tokens _to will be the ZERO_ADDRESS.
+   * @param _amount uint256 The amount to transfer.
    * @return bool true if the transfer is authorized, false if it would revert.
    */
   function isTransferAllowed(
     address _operator,
     address _from,
     address _to,
-    uint256 _value
+    uint256 _amount
   ) external view
     returns (bool);
 
   /**
-   * Checks the balance available for a user, this may be less than or equal to
-   * their total token balance.
+   * @return uint256 the balance available for a user, this may be less than or
+   * equal to their total token balance.
    */
   function availableBalanceOf(
     address _from
