2d25b93652172f8898a7a0935240d4dfb22a5063
hardlydifficult <hardlydiff@gmail.com>
2019-06-10 18:29:10 -0700

    Comment updates

--- contracts/IAuthorization.sol
+++ contracts/IAuthorization.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.5.0;
 
+/**
+  * Check if a transfer should be allowed to proceed.
+  * @param _operator address The account initiating this transfer, may be the same as `_from`
+  * @param _from address The account holding the tokens to be sent and initiating this transfer.
+  * @param _to address The account of the recipient. When selling tokens _to will be the ZERO_ADDRESS.
+  * @param _value uint256 The amount to transfer.
+  */
 interface IAuthorization {
   /**
-   * @notice Check if an account is approved to transfer tokens to account `to` of an amount `value`.
-   * Reverts the transaction if the transfer is not approved.
-   * @param _operator address The account initiating this transfer, may be the same as `_from`
-   * @param _from address The account holding the tokens to be sent and initiating this transfer.
-   * @param _to address The account of the recipient. When selling tokens _to will be the ZERO_ADDRESS.
-   * @param _value uint256 The amount to transfer.
+   * Reverts if the transfer is not approved.
    */
   function authorizeTransfer(
     address _operator,
@@ -16,6 +18,9 @@ interface IAuthorization {
     uint256 _value
   ) external;
 
+  /**
+   * @return bool true if the transfer is authorized, false if it would revert.
+   */
   function isTransferAllowed(
     address _operator,
     address _from,
@@ -24,6 +29,10 @@ interface IAuthorization {
   ) external view
     returns (bool);
 
+  /**
+   * Checks the balance available for a user, this may be less than or equal to
+   * their total token balance.
+   */
   function availableBalanceOf(
     address _from
   ) external view
