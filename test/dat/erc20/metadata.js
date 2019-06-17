358ee6a27e025dc464513f537ad219cd6510189a
hardlydifficult <hardlydiff@gmail.com>
2019-06-17 10:50:49 -0700

    Test updates

--- test/dat/erc20/metadata.js
+++ test/dat/erc20/metadata.js
@@ -1,9 +1,10 @@
-const { deployDat } = require('../../helpers');
+const { deployDat, shouldFail } = require('../../helpers');
 
-contract('dat / erc20 / metadata', () => {
+contract('dat / erc20 / metadata', (accounts) => {
   const name = 'Token Name';
   const symbol = 'SBL';
   let dat;
+  let tx;
 
   before(async () => {
     dat = await deployDat({ name, symbol });
@@ -14,10 +15,34 @@ contract('dat / erc20 / metadata', () => {
   });
 
   it('should have a symbol', async () => {
-    assert.equal(await dat.symbol, symbol);
+    assert.equal(await dat.symbol(), symbol);
   });
 
   it('should have 18 decimals', async () => {
-    assert.equal(await dat.decimals, 18);
+    assert.equal(await dat.decimals(), 18);
   });
+
+  describe('updateName', () => {
+    describe('can change name', () => {
+      const newName = 'New Name';
+
+      before(async () => {
+        tx = await dat.updateName(newName);
+      });
+
+      it('should have the new name', async () => {
+        assert.equal(await dat.name(), newName);
+      });
+
+      it('should emit an event', async () => {
+        console.log(tx);
+      });
+    });
+
+    it('should fail to change name from a different account', async () => {
+      await shouldFail(dat.updateName('Test', { from: accounts[2] }), 'CONTROL_ONLY');
+    });
+  });
+
+  describe('updateSymbol');
 });
