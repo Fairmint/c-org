a515fef5ed2b1251c2a2e314df482c5efbfa0336
hardlydifficult <hardlydiff@gmail.com>
2019-06-17 16:40:45 -0700

    Stub simple tests

---
+++ test/dat/erc20/totalSupply.js
@@ -0,0 +1,19 @@
+const { deployDat } = require('../../helpers');
+
+contract('dat / erc20 / totalSupply', () => {
+  let dat;
+  const initReserve = 42;
+
+  before(async () => {
+    dat = await deployDat({ initReserve });
+  });
+
+  it('defaults to initReserve', async () => {
+    assert.equal((await dat.totalSupply()).toString(), initReserve);
+  });
+
+  it('goes up on buy');
+  it('goes down on sell');
+  it('goes down on burn');
+  it('no change on transfer');
+});
