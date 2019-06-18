bf1127bca090523433b38b34b79594a016fe8aed
hardlydifficult <hardlydiff@gmail.com>
2019-06-17 22:43:00 -0700

    Update migrations

--- migrations/4_dat.js
+++ migrations/4_dat.js
@@ -3,8 +3,6 @@ const datArtifact = artifacts.require('DecentralizedAutonomousTrust');
 module.exports = function deployDat(deployer) {
   deployer.deploy(
     datArtifact,
-    'Fairmint', // name
-    'FSE', // symbol
     '42000000000000000000', // initReserve
     '0x0000000000000000000000000000000000000000', // currencyAddress
     '0', // initGoal
