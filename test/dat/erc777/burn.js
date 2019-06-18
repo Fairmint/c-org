fd3058919c7c4a512e3fc0778dbcd0b649592fad
hardlydifficult <hardlydiff@gmail.com>
2019-06-18 15:14:57 -0700

    Simple burn test

---
+++ test/dat/erc777/burn.js
@@ -0,0 +1,25 @@
+const { deployDat } = require('../../helpers');
+
+contract('dat / erc20 / balanceOf', (accounts) => {
+  let dat;
+  const initReserve = 42;
+
+  before(async () => {
+    dat = await deployDat({ initReserve });
+  });
+
+  describe('can burn initReserve', () => {
+    let accountBalanceBefore;
+
+    before(async () => {
+      accountBalanceBefore = await dat.balanceOf(accounts[0]);
+      await dat.burn(42);
+    });
+
+    it('account balance went down', async () => {
+      assert.equal(await dat.balanceOf(accounts[0]), accountBalanceBefore.sub(42));
+    });
+  });
+
+  it('can burn after buy');
+});
