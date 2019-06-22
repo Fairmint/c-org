30dd9b785d4a263c8849685b04a67b2ffe55973f
hardlydifficult <hardlydiff@gmail.com>
2019-06-21 17:59:56 -0700

    Restore basic auth test

---
+++ test/dat/auth/canBlockBuy.js
@@ -0,0 +1,84 @@
+/**
+ * Tests buying when not authorized.
+ */
+
+const { deployDat, shouldFail, updateDatConfig } = require("../helpers");
+
+const authArtifact = artifacts.require("Authorization_Pausable");
+
+contract("dat / auth / canBlockBuy", accounts => {
+  let dat;
+  let fse;
+  let auth;
+
+  before(async () => {
+    [dat, fse] = await deployDat({
+      initGoal: 99999
+    });
+    auth = await authArtifact.new(dat.address);
+    await updateDatConfig(
+      dat,
+      fse,
+      { authorizationAddress: auth.address },
+      accounts[0]
+    );
+  });
+
+  it("balanceOf should be 0 by default", async () => {
+    const balance = await fse.balanceOf(accounts[1]);
+
+    assert.equal(balance, 0);
+  });
+
+  describe("can buy tokens", () => {
+    before(async () => {
+      await dat.buy(accounts[1], "100000000000000000000", 1, {
+        value: "100000000000000000000",
+        from: accounts[1]
+      });
+    });
+
+    it("balanceOf should have increased", async () => {
+      const balance = await fse.balanceOf(accounts[1]);
+
+      assert.equal(balance.toString(), "200002000020000200002");
+    });
+
+    describe("when blocked", () => {
+      before(async () => {
+        await auth.setAuthorized(false);
+      });
+
+      it("should fail to buy tokens", async () => {
+        await shouldFail(
+          dat.buy(accounts[1], "100000000000000000000", 1, {
+            value: "100000000000000000000",
+            from: accounts[1]
+          })
+        );
+      });
+
+      it("balanceOf should not have changed", async () => {
+        const balance = await fse.balanceOf(accounts[1]);
+
+        assert.equal(balance.toString(), "200002000020000200002");
+      });
+
+      describe("can buy tokens on the 3rd attempt", () => {
+        before(async () => {
+          await auth.setAuthorized(true);
+          await dat.buy(accounts[1], "100000000000000000000", 1, {
+            value: "100000000000000000000",
+            from: accounts[1]
+          });
+        });
+
+        it("balanceOf should have increased", async () => {
+          const balance = await fse.balanceOf(accounts[1]);
+
+          assert.equal(balance.toString(), "400004000040000400004");
+        });
+      });
+    });
+  });
+});
