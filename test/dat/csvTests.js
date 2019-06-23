fba215e922f36d18a7f22a79be2d262fd79ced2a
hardlydifficult <hardlydiff@gmail.com>
2019-06-23 12:21:11 -0700

    +set initial balance

--- test/dat/csvTests.js
+++ test/dat/csvTests.js
@@ -1,6 +1,7 @@
-const { deployDat } = require("../helpers");
+const { deployDat, constants } = require("../helpers");
 const Papa = require("papaparse");
 const fs = require("fs");
+const BigNumber = require("bignumber.js");
 
 contract("dat / csvTests", accounts => {
   let dat;
@@ -30,10 +31,32 @@ contract("dat / csvTests", accounts => {
       ),
       { header: true }
     );
+    for (let i = 0; i < balanceJson.length; i++) {
+      const row = balanceJson[i];
+      await setBalance(accounts[parseInt(row.AccId)], row.InitialBalance);
+    }
     console.log(configJson.data);
-    console.log(balanceJson.data);
     console.log(sheetJson.data);
   });
 
   it("todo", async () => {});
 });
+
+async function setBalance(account, targetBalance) {
+  console.log(`Set ${account} to ${targetBalance.toFixed()}`);
+  targetBalance = new BigNumber(targetBalance);
+  const currentBalance = new BigNumber(
+    web3.eth.fromWei(await web3.eth.getBalance(account), "ether")
+  );
+  const burnAmount = targetBalance.minus(currentBalance);
+  console.log(
+    `Current balance ${currentBalance.toFixed()}, target ${targetBalance.toFixed()}, burn ${burnAmount.toFixed()}`
+  );
+  await web3.eth.sendTransaction({
+    from: account,
+    to: constants.ZERO_ADDRESS,
+    value: web3.eth.toWei(burnAmount.toFixed(), "ether")
+  });
+  const finalBalance = new BigNumber(await web3.eth.getBalance(account));
+  console.log(`Final balance ${finalBalance.toFixed()}`);
+}
