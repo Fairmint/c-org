9b1383a245053739f094cf75ed8b3a03d116dc33
hardlydifficult <hardlydiff@gmail.com>
2019-06-23 12:25:21 -0700

    Test fixes

--- test/dat/csvTests.js
+++ test/dat/csvTests.js
@@ -16,47 +16,47 @@ contract("dat / csvTests", accounts => {
         `${__dirname}/test-data/buy_sell_no-pre-mint Configuration.csv`,
         "utf8"
       )
-    );
+    ).data;
     const balanceJson = Papa.parse(
       fs.readFileSync(
         `${__dirname}/test-data/buy_sell_no-pre-mint InitBalances.csv`,
         "utf8"
       ),
       { header: true }
-    );
+    ).data;
     const sheetJson = Papa.parse(
       fs.readFileSync(
         `${__dirname}/test-data/buy_sell_no-pre-mint Script.csv`,
         "utf8"
       ),
       { header: true }
-    );
+    ).data;
     for (let i = 0; i < balanceJson.length; i++) {
       const row = balanceJson[i];
       await setBalance(accounts[parseInt(row.AccId)], row.InitialBalance);
     }
-    console.log(configJson.data);
-    console.log(sheetJson.data);
+    console.log(configJson);
+    console.log(sheetJson);
   });
 
   it("todo", async () => {});
 });
 
 async function setBalance(account, targetBalance) {
-  console.log(`Set ${account} to ${targetBalance.toFixed()}`);
+  console.log(`Set ${account} to ${targetBalance}`);
   targetBalance = new BigNumber(targetBalance);
   const currentBalance = new BigNumber(
-    web3.eth.fromWei(await web3.eth.getBalance(account), "ether")
+    web3.utils.fromWei(await web3.eth.getBalance(account), "ether")
   );
   const burnAmount = targetBalance.minus(currentBalance);
   console.log(
-    `Current balance ${currentBalance.toFixed()}, target ${targetBalance.toFixed()}, burn ${burnAmount.toFixed()}`
+    `Current balance ${currentBalance.toFormat()}, target ${targetBalance.toFormat()}, burn ${burnAmount.toFormat()}`
   );
   await web3.eth.sendTransaction({
     from: account,
     to: constants.ZERO_ADDRESS,
-    value: web3.eth.toWei(burnAmount.toFixed(), "ether")
+    value: web3.utils.toWei(burnAmount.toFixed(), "ether")
   });
   const finalBalance = new BigNumber(await web3.eth.getBalance(account));
-  console.log(`Final balance ${finalBalance.toFixed()}`);
+  console.log(`Final balance ${finalBalance.toFormat()}`);
 }
