1660c0c36973a57068908a6c527a19dc3393f53b
hardlydifficult <hardlydiff@gmail.com>
2019-06-23 11:59:37 -0700

    +fs

--- test/dat/csvTests.js
+++ test/dat/csvTests.js
@@ -1,5 +1,6 @@
 const { deployDat } = require("../helpers");
 const Papa = require("papaparse");
+const fs = require("fs");
 
 contract("dat / csvTests", accounts => {
   let dat;
@@ -10,19 +11,19 @@ contract("dat / csvTests", accounts => {
       initGoal: "1000000000000000000000"
     });
     const configJson = Papa.parse(
-      "./test-data/buy_sell_no-pre-mint Configuration.csv"
+      fs.readFileSync("./test-data/buy_sell_no-pre-mint Configuration.csv")
     );
     const balanceJson = Papa.parse(
-      "./test-data/buy_sell_no-pre-mint InitBalances.csv",
+      fs.readFileSync("./test-data/buy_sell_no-pre-mint InitBalances.csv"),
       { headers: true }
     );
     const sheetJson = Papa.parse(
-      "./test-data/buy_sell_no-pre-mint Script.csv",
+      fs.readFileSync("./test-data/buy_sell_no-pre-mint Script.csv"),
       { headers: true }
     );
-    console.log(configJson);
-    console.log(balanceJson);
-    console.log(sheetJson);
+    console.log(configJson.data);
+    console.log(balanceJson.data);
+    console.log(sheetJson.data);
   });
 
   it("todo", async () => {});
