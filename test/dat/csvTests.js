298f4dfdf4e8679b7416f524f5e2fe2eea0c9b30
hardlydifficult <hardlydiff@gmail.com>
2019-06-24 21:12:55 -0700

    +shift

--- test/dat/csvTests.js
+++ test/dat/csvTests.js
@@ -316,7 +316,7 @@ async function setBalanceAndApprove(accountId, targetBalance) {
   assert.equal(balance.toFixed(), targetBalance.toFixed());
 
   if (account == beneficiary) {
-    spentByBeneficiary = spentByBeneficiary.minus(targetBalance);
+    spentByBeneficiary = spentByBeneficiary.minus(targetBalance.shiftedBy(18));
   }
 
   // TODO for ETH support (but need to deal with gas costs as well - maybe detect and refund gas for simplicity?)
