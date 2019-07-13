039571b519d9fc2c76f00269667e4d430d4a07a1
NickCuso <hardlydiff@gmail.com>
2019-07-12 17:49:21 -0700

    Deploy to rinkeby

--- migrations/2_deploy_and_configure.js
+++ migrations/2_deploy_and_configure.js
@@ -1,54 +1,113 @@
 const erc1820 = require("erc1820");
 const { updateDatConfig } = require("../helpers");
+const fs = require("fs");
 
 // TODO: replace with real TPL artifact
 const tplArtifact = artifacts.require("TestTPLAttributeRegistry");
+// TODO: replace with real DAI
+const testDaiArtifact = artifacts.require("TestDai");
 const authArtifact = artifacts.require("Authorization");
 const fairArtifact = artifacts.require("FAIR");
 const datArtifact = artifacts.require("DecentralizedAutonomousTrust");
 const bigDivArtifact = artifacts.require("BigDiv");
 const vestingArtifact = artifacts.require("Vesting");
+const proxyArtifact = artifacts.require("UpgradeableProxy");
+const proxyAdminArtifact = artifacts.require("UpgradeableProxyAdmin");
 
 module.exports = async function deployAndConfigure(
   deployer,
   network,
   accounts
 ) {
+  const json = {};
+  const control = accounts[0];
+
   // Deploy 1820 (for local testing only)
   if (network === "development") {
     await erc1820.deploy(web3);
   }
 
+  // Deploy proxy admin
+  const proxyAdmin = await proxyAdminArtifact.new();
+  json.proxyAdmin = {
+    address: proxyAdmin.address,
+    abi: proxyAdmin.abi
+  };
+
   // Deploy token
-  // TODO upgradable?
-  const fair = await deployer.deploy(fairArtifact);
+  const fairContract = await deployer.deploy(fairArtifact);
+  const fairProxy = await proxyArtifact.new(
+    fairContract.address, // logic
+    proxyAdmin.address, // admin
+    [] // data
+  );
+  const fair = await fairArtifact.at(fairProxy.address);
+  json.fair = {
+    address: fair.address,
+    implementation: fairContract.address,
+    abi: fair.abi
+  };
 
   // Deploy Library
   const bigDiv = await deployer.deploy(bigDivArtifact);
+  json.bigDiv = {
+    address: bigDiv.address,
+    abi: bigDiv.abi
+  };
+
+  // Deploy testDAI
+  const dai = await deployer.deploy(testDaiArtifact);
+  json.dai = {
+    address: dai.address,
+    abi: dai.abi
+  }
 
   // Deploy Dat
-  // TODO upgradable
-  const dat = await deployer.deploy(datArtifact);
+  const datContract = await deployer.deploy(datArtifact);
+  const datProxy = await proxyArtifact.new(
+    datContract.address, // logic
+    proxyAdmin.address, // admin
+    [] // data
+  );
+  const dat = await datArtifact.at(datProxy.address);
   await dat.initialize(
     bigDiv.address,
-    fairArtifact.address,
+    fair.address,
     "42000000000000000000", // initReserve
-    "0x0000000000000000000000000000000000000000", // currencyAddress
+    dai.address, // currencyAddress
     "0", // initGoal
     "1", // buySlopeNum
-    "100000", // buySlopeDen
+    "100000000000000000000", // buySlopeDen
     "1000", // investmentReserveBasisPoints
     "1000" // revenueCommitementBasisPoints
   );
+  json.dat = {
+    address: dat.address,
+    implementation: datContract.address
+  };
 
   // Deploy TPL
   const tpl = await deployer.deploy(tplArtifact);
+  json.tpl = {
+    address: tpl.address,
+    abi: tpl.abi
+  };
 
   // Deploy auth
-  // TODO upgradable
-  const auth = await deployer.deploy(authArtifact);
+  const authContract = await deployer.deploy(authArtifact);
+  const authProxy = await proxyArtifact.new(
+    authContract.address, // logic
+    proxyAdmin.address, // admin
+    [] // data
+  );
+  const auth = await authArtifact.at(authProxy.address);
   await auth.initialize(fair.address);
   await auth.updateAuth(tpl.address, [42], [0, 0], [0, 0, 0]);
+  json.auth = {
+    address: auth.address,
+    implementation: authContract.address,
+    abi: auth.abi
+  };
 
   // Update dat with auth (and other settings)
   await updateDatConfig(
@@ -72,14 +131,28 @@ module.exports = async function deployAndConfigure(
     200, // duration in seconds
     false // non-revocable
   );
-  const vestingAccount1 = await deployer.deploy(
-    vestingArtifact,
-    accounts[1], // beneficiary
-    Math.round(Date.now() / 1000) + 100, // startTime is seconds
-    120, // cliffDuration in seconds
-    200, // duration in seconds
-    false // non-revocable
-  );
+  // const vestingAccount1 = await deployer.deploy(
+  //   vestingArtifact,
+  //   accounts[1], // beneficiary
+  //   Math.round(Date.now() / 1000) + 100, // startTime is seconds
+  //   120, // cliffDuration in seconds
+  //   200, // duration in seconds
+  //   false // non-revocable
+  // );
   await fair.transfer(vestingBeneficiary.address, "40000000000000000000");
-  await fair.transfer(vestingAccount1.address, "2000000000000000000");
+  //await fair.transfer(vestingAccount1.address, "2000000000000000000");
+  json.vesting = {
+    beneficiary: {
+      address: vestingBeneficiary.address
+    },
+    // account1: {
+    //   address: vestingAccount1.address
+    // },
+    abi: vestingBeneficiary.abi // same for all
+  };
+
+  // Test the upgrade process
+  //await proxyAdmin.upgrade(fairProxy.address, fairContract.address);
+
+  fs.writeFile(`contracts_${network}.json`, JSON.stringify(json, null, 2), () => {});
 };
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,14 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const privateKeyOrMnemonic =
+  "todo";
 
 module.exports = {
   /**
@@ -48,7 +51,7 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
 
     // Another network with more advanced options...
     // advanced: {
@@ -59,17 +62,20 @@ module.exports = {
     // from: <address>,        // Account to send txs from (default: accounts[0])
     // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
-
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKeyOrMnemonic,
+          `https://rinkeby.infura.io/v3/c13019f552f542188c0b0cdfaeb76cc4`
+        ),
+      network_id: "4", // Network Id for Rinkeby
+      confirmations: 1,
+      gas: 6400000,
+      gasPrice: 5000000000, // 5GWEI
+      skipDryRun: true
+    }
 
     // Useful for private networks
     // private: {
