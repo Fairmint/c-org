b07344ecab81862a8b91c238c29da4a74baa647a
Hardly Difficult <hardlydiff@gmail.com>
2019-07-22 15:01:39 -0700

    Restore the gas reporter (#129)

--- truffle-config.js
+++ truffle-config.js
@@ -103,7 +103,8 @@ module.exports = {
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000,
-    enableTimeouts: false
+    enableTimeouts: false,
+    reporter: "eth-gas-reporter"
   },
 
   // Configure your compilers
