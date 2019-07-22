94a69dad5047fc59760d5c6e987de3fa7ad66495
Hardly Difficult <hardlydiff@gmail.com>
2019-07-22 10:57:56 -0700

    NPM abi package (#122)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,9 @@ const fs = require("fs");
 
 const testDaiArtifact = artifacts.require("TestDai");
 const erc1820Artifact = artifacts.require("IERC1820Registry");
+const vestingArtifact = artifacts.require("Vesting");
 
 contract("deploy script", (accounts, network) => {
-  const json = {};
-
   it("deploy", async () => {
     // Deploy 1820 (for local testing only)
     if (network === "development") {
@@ -35,54 +34,37 @@ contract("deploy script", (accounts, network) => {
       ]
     });
 
-    json.erc1820 = {
-      address: "0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24",
-      abi: erc1820Artifact.abi
-    };
-    json.proxyAdmin = {
-      address: contracts.proxyAdmin.address,
-      abi: contracts.proxyAdmin.abi
-    };
-    json.fair = {
-      address: contracts.fair.address,
-      abi: contracts.fair.abi
-    };
-    json.bigDiv = {
-      address: contracts.bigDiv.address,
-      abi: contracts.bigDiv.abi
-    };
-    json.currencyToken = {
-      address: currencyToken.address, // TODO drop addresses for clarity?
-      abi: currencyToken.abi
-    };
-    json.dat = {
-      address: contracts.dat.address,
-      abi: contracts.dat.abi
-    };
-    json.tpl = {
-      address: contracts.tpl.address,
-      abi: contracts.tpl.abi
-    };
-    json.auth = {
-      address: contracts.auth.address,
-      abi: contracts.auth.abi
-    };
-    if (contracts.vesting) {
-      json.vesting = {
-        abi: contracts.vesting[0].abi, // same for all
-        accounts: []
-      };
-      for (let i = 0; i < contracts.vesting.length; i++) {
-        json.vesting.accounts.push(contracts.vesting[i].address);
-      }
-    }
+    const abiJson = {};
+    abiJson.erc1820 = erc1820Artifact.abi;
+    // TODO address: "0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24",
+    abiJson.proxyAdmin = contracts.proxyAdmin.abi;
+    //address: contracts.proxyAdmin.address,
+    abiJson.fair = contracts.fair.abi;
+    //address: contracts.fair.address,
+    abiJson.bigDiv = contracts.bigDiv.abi;
+    //address: contracts.bigDiv.address,
+    abiJson.erc20 = currencyToken.abi;
+    // currencyToken.address, // TODO drop addresses for clarity?
+    // TODO these contracts will be removed
+    // abiJson.dat = contracts.dat.abi;
+    // // address: contracts.dat.address,
+    // abiJson.tpl = contracts.tpl.abi;
+    // // address: contracts.tpl.address,
+    // abiJson.auth = contracts.auth.abi;
+    // address: contracts.auth.address,
+    abiJson.vesting = vestingArtifact.abi;
+    // if (contracts.vesting) {
+    //   for (let i = 0; i < contracts.vesting.length; i++) {
+    //     abiJson.vesting.accounts.push(contracts.vesting[i].address);
+    //   }
+    // }
 
     // Test the upgrade process
     //await proxyAdmin.upgrade(fairProxy.address, fairContract.address);
 
     fs.writeFile(
-      `contracts_${network}.json`,
-      JSON.stringify(json, null, 2),
+      `c-org-abi/abi.json`,
+      JSON.stringify(abiJson, null, 2),
       () => {}
     );
   });
