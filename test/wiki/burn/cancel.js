6fb4c0c9517fa3c3d7dcd36fff805237a77443fd
NickCuso <HardlyDiff@gmail.com>
2019-08-02 15:21:40 -0700

    Burn wiki tests

---
+++ test/wiki/burn/cancel.js
@@ -0,0 +1,36 @@
+const { constants, deployDat, shouldFail } = require("../../helpers");
+
+contract("wiki / burn / cancel", accounts => {
+  let contracts;
+  const investor = accounts[3];
+  const burnAmount = "42";
+
+  before(async () => {
+    contracts = await deployDat(accounts, {
+      initGoal: "10000000000000000000000"
+    });
+
+    // Buy tokens for various accounts
+    for (let i = 0; i < 9; i++) {
+      await contracts.dat.buy(accounts[i], "100000000000000000000", 1, {
+        value: "100000000000000000000",
+        from: accounts[i]
+      });
+    }
+
+    await contracts.dat.close({ from: accounts[0] });
+  });
+
+  it("Sanity check: state is cancel", async () => {
+    const state = await contracts.dat.state();
+    assert.equal(state.toString(), constants.STATE.CANCEL);
+  });
+
+  it("Burn fails", async () => {
+    await shouldFail(
+      contracts.fair.burn(burnAmount, [], {
+        from: investor
+      })
+    );
+  });
+});
