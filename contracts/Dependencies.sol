983d98c2b3d11deefe3230b354de4651504cc6bf
Hardly Difficult <hardlydiff@gmail.com>
2019-08-23 17:32:14 -0700

    Simplify dependencies.sol

--- contracts/Dependencies.sol
+++ contracts/Dependencies.sol
@@ -3,15 +3,3 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/drafts/TokenVesting.sol";
 import "zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
 import "zos-lib/contracts/upgradeability/ProxyAdmin.sol";
-
-
-/**
- * This creates the artifacts allowing us to use these 3rd party contracts directly
- */
-contract Dependencies is TokenVesting, AdminUpgradeabilityProxy, ProxyAdmin
-{
-  constructor() internal
-    TokenVesting(address(0), 0, 0, 0, false)
-    AdminUpgradeabilityProxy(address(0), address(0), '')
-  {}
-}
