8237384d1beaafa669b1643f1e665dd1b7fd80a4
NickCuso <hardlydiff@gmail.com>
2019-09-06 15:19:57 -0700

    Test cleanup

--- csvTests/csvTests.js
+++ csvTests/csvTests.js
@@ -186,17 +186,6 @@ contract("dat / csvTests", accounts => {
 
       if (currency) {
         if (
-          currency.isOperatorFor &&
-          !(await currency.isOperatorFor(
-            contracts.dat.address,
-            row.account.address
-          ))
-        ) {
-          console.log(`  Set #${row.account.id} to authorize dat`);
-          await currency.authorizeOperator(contracts.dat.address, {
-            from: row.account.address
-          });
-        } else if (
           currency.allowance &&
           (await currency.allowance(
             row.account.address,
@@ -332,7 +321,6 @@ contract("dat / csvTests", accounts => {
         case "xfer":
           if (isCurrency) {
             if (currency) {
-              if (currency.transfer) {
                 tx = await currency.transfer(
                   targetAddress,
                   quantity.toFixed(),
@@ -340,16 +328,6 @@ contract("dat / csvTests", accounts => {
                     from: row.account.address
                   }
                 );
-              } else {
-                tx = await currency.send(
-                  targetAddress,
-                  quantity.toFixed(),
-                  web3.utils.asciiToHex(""),
-                  {
-                    from: row.account.address
-                  }
-                );
-              }
             } else {
               tx = await web3.eth.sendTransaction({
                 from: row.account.address,
