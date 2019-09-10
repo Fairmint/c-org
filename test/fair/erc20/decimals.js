81544633149079d872e3d83e8eba701fc92d0ebd
NickCuso <hardlydiff@gmail.com>
2019-09-09 18:43:45 -0700

    lint

--- test/fair/erc20/decimals.js
+++ test/fair/erc20/decimals.js
@@ -1,6 +1,6 @@
 const { shouldFail, deployDat, updateDatConfig } = require("../../helpers");
 
-contract("fair / erc20 / decimals", (accounts) => {
+contract("fair / erc20 / decimals", accounts => {
   let contracts;
 
   before(async () => {
