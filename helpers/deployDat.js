c32e64503f3a9ce05b23dd113f5729adacc2aa4b
NickCuso <hardlydiff@gmail.com>
2019-10-11 17:57:29 -0700

    Fix typo

--- helpers/deployDat.js
+++ helpers/deployDat.js
@@ -17,7 +17,7 @@ module.exports = async function deployDat(accounts, options, useProxy = true) {
       buySlopeNum: "1",
       buySlopeDen: "100000000000000000000",
       investmentReserveBasisPoints: "1000",
-      revenueCommitementBasisPoints: "1000",
+      revenueCommitmentBasisPoints: "1000",
       control: accounts.length > 2 ? accounts[1] : accounts[0],
       beneficiary: accounts[0],
       feeCollector: accounts.length > 2 ? accounts[2] : accounts[0]
