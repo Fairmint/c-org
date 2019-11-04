ca08785ffce72a519d040dd74519b7033de80191
Hardly Difficult <hardlydiff@gmail.com>
2019-11-04 11:42:23 -0800

    Migrate Whitelist.sol (#273)
    * Update dependencies
    
    * Migrate Whitelist to solidity
    
    * Bump solc
    
    * Typo

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,100 @@
+pragma solidity 0.5.12;
+
+
+import "./interfaces/ERC1404.sol";
+
+/**
+ * @title ERC-1404 whitelist implementation which manages KYC approvals for the org.
+ */
+contract Whitelist is ERC1404
+{
+  /**
+   * Emits when an operator KYC approves (or revokes) a trader.
+   */
+  event Approve(address indexed _trader, bool _isApproved);
+
+  mapping(address => bool) public approved;
+  address public dat;
+  address public owner;
+
+  /**
+   * @notice Called once to complete configuration for this contract.
+   * @dev Done with `initialize` instead of a constructor in order to support
+   * using this contract via an Upgradable Proxy.
+   */
+  function initialize(
+    address _dat
+  ) external
+  {
+    require(owner == address(0), "ALREADY_INITIALIZED");
+
+    dat = _dat;
+    owner = msg.sender;
+
+    // Setting 0 to approved allows mint/burn actions
+    approved[address(0)] = true;
+  }
+
+  function detectTransferRestriction(
+    address _from,
+    address _to,
+    uint256 //_value (ignored)
+  ) public view returns(uint8)
+  {
+    if(approved[_from] && approved[_to])
+    {
+      // Transfer approved
+      return 0;
+    }
+
+    // Transfer denied
+    return 1;
+  }
+
+  function messageForTransferRestriction(
+    uint8 _restrictionCode
+  ) external pure
+    returns (string memory)
+  {
+    if(_restrictionCode == 0)
+    {
+      return "SUCCESS";
+    }
+    if(_restrictionCode == 1)
+    {
+      return "DENIED";
+    }
+    return "UNKNOWN_ERROR";
+  }
+
+  /**
+   * @notice Allows the owner of this contract to approve (or deny)
+   * a trader's account.
+   */
+  function approve(
+    address _trader,
+    bool _isApproved
+  ) external
+  {
+    require(msg.sender == owner, "OWNER_ONLY");
+
+    approved[_trader] = _isApproved;
+    emit Approve(_trader, _isApproved);
+  }
+
+  /**
+   * @notice Called by the DAT contract before a transfer occurs.
+   * @dev This call will revert when the transfer is not authorized.
+   */
+  function authorizeTransfer(
+    address _from,
+    address _to,
+    uint256 _value,
+    bool _isSell
+  ) external
+  {
+    require(dat == msg.sender, "CALL_VIA_DAT_ONLY");
+    require(detectTransferRestriction(_from, _to, _value) == 0, "TRANSFER_DENIED");
+    // Future versions will add additional logic here
+  }
+}
