a6490322e06d3a43dd24f17a79dfa5df1360fc85
Hardly Difficult <hardlydiff@gmail.com>
2019-11-28 15:01:58 -0800

    Switch the whitelist reference to use an interface (#331)

--- contracts/interfaces/IWhitelist.sol
+++ contracts/interfaces/IWhitelist.sol
@@ -4,8 +4,9 @@ pragma solidity 0.5.13;
 /**
  * Source: https://raw.githubusercontent.com/simple-restricted-token/reference-implementation/master/contracts/token/ERC1404/ERC1404.sol
  * With ERC-20 APIs removed (will be implemented as a separate contract).
+ * And adding authorizeTransfer.
  */
-interface ERC1404
+interface IWhitelist
 {
   /**
    * @notice Detects if a transfer will be reverted and if so returns an appropriate reference code
@@ -32,4 +33,17 @@ interface ERC1404
     uint8 restrictionCode
   ) external pure
     returns (string memory);
+
+  /**
+   * @notice Called by the DAT contract before a transfer occurs.
+   * @dev This call will revert when the transfer is not authorized.
+   * This is a mutable call to allow additional data to be recorded,
+   * such as when the user aquired their tokens.
+   */
+  function authorizeTransfer(
+    address _from,
+    address _to,
+    uint _value,
+    bool _isSell
+  ) external;
 }
