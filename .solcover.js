25ec9253cace10537776b8409310d5688d251bb6
Hardly Difficult <hardlydiff@gmail.com>
2019-12-11 12:39:45 -0800

    solcover: istanbul

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,8 @@ module.exports = {
   skipFiles: ['Dependencies.sol', 'Migrations.sol', 'test-artifacts/', 'interfaces/'],
   providerOptions: {
     total_accounts: 100,
-    default_balance_ether: 10000000000
+    default_balance_ether: 10000000000,
+    hardfork: 'istanbul'
   },
   mocha: {
     reporter: null
