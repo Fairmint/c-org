43cdfe238627cae1e2d467e0e33cfb8c28e0d885
Hardly Difficult <hardlydiff@gmail.com>
2019-12-28 02:28:33 -0800

    Rebuild contracts (#367)

--- truffle-config.js
+++ truffle-config.js
@@ -140,7 +140,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.14", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.15", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {
         // See the solidity docs for advice about optimization and evmVersion
