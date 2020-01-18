91d06696d5db6612cf2c2d884bb067b880c1daff
NickCuso <hardlydiff@gmail.com>
2020-01-17 16:29:06 -0800

    Add permit, update to 0.5.16

--- contracts/DecentralizedAutonomousTrust.sol
+++ contracts/DecentralizedAutonomousTrust.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.15;
+pragma solidity 0.5.16;
 
 import "./interfaces/IWhitelist.sol";
 import "./math/BigDiv.sol";
@@ -180,6 +180,14 @@ contract DecentralizedAutonomousTrust
   /// @notice The current state of the contract.
   /// @dev See the constants above for possible state values.
   uint public state;
+
+  string public constant version = "2";
+  // --- EIP712 niceties ---
+  // Original source: https://etherscan.io/address/0x6b175474e89094c44da98b954eedeac495271d0f#code
+  mapping (address => uint) public nonces;
+  bytes32 public DOMAIN_SEPARATOR;
+  // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)");
+  bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
 
   modifier authorizeTransfer(
     address _from,
@@ -403,6 +411,30 @@ contract DecentralizedAutonomousTrust
       initReserve = _initReserve;
       _mint(beneficiary, initReserve);
     }
+  }
+  function getChainId(
+  ) private pure
+    returns (uint id)
+  {
+    // solium-disable-next-line
+    assembly
+    {
+      id := chainid()
+    }
+  }
+
+  function initializePermit(
+  ) public
+  {
+    DOMAIN_SEPARATOR = keccak256(
+      abi.encode(
+        keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+        keccak256(bytes(name())),
+        keccak256(bytes(version)),
+        getChainId(),
+        address(this)
+      )
+    );
   }
 
   function updateConfig(
@@ -972,5 +1004,42 @@ contract DecentralizedAutonomousTrust
     }
 
     emit Close(exitFee);
+  }
+
+  // --- Approve by signature ---
+  // Original source: https://etherscan.io/address/0x6b175474e89094c44da98b954eedeac495271d0f#code
+  function permit(
+    address holder,
+    address spender,
+    uint256 nonce,
+    uint256 expiry,
+    bool allowed,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external
+  {
+    bytes32 digest = keccak256(
+      abi.encodePacked(
+        "\x19\x01",
+        DOMAIN_SEPARATOR,
+        keccak256(
+          abi.encode(PERMIT_TYPEHASH,
+                    holder,
+                    spender,
+                    nonce,
+                    expiry,
+                    allowed
+          )
+        )
+      )
+    );
+
+    require(holder != address(0), "DAT/invalid-address-0");
+    require(holder == ecrecover(digest, v, r, s), "DAT/invalid-permit");
+    require(expiry == 0 || now <= expiry, "DAT/permit-expired");
+    require(nonce == nonces[holder]++, "DAT/invalid-nonce");
+    uint wad = allowed ? uint(-1) : 0;
+    _approve(holder, spender, wad);
   }
 }
