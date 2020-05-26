a654eb5affd26b056dffb4945929904739b719fc
NickCuso <hardlydiff@gmail.com>
2020-05-25 20:43:58 -0700

    lint

--- contracts/DecentralizedAutonomousTrust.sol
+++ contracts/DecentralizedAutonomousTrust.sol
@@ -1086,7 +1086,8 @@ contract DecentralizedAutonomousTrust
     uint8 v,
     bytes32 r,
     bytes32 s
-  ) external {
+  ) external
+  {
     require(deadline >= block.timestamp, "EXPIRED");
     bytes32 digest = keccak256(
       abi.encodePacked(
