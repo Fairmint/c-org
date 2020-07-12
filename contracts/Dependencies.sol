82e9e58bb37971dc50f0f929b604d04d4030c884
NickCuso <hardlydiff@gmail.com>
2020-07-12 12:14:07 -0700

    Simplify

--- contracts/Dependencies.sol
+++ contracts/Dependencies.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/drafts/TokenVesting.sol";
-import "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
-import "@openzeppelin/upgrades/contracts/upgradeability/ProxyAdmin.sol";
-
 /**
  * This creates the artifacts allowing us to use these 3rd party contracts directly
  */
-// solium-disable-next-line
-contract Dependencies
-{}
+
+import "@openzeppelin/contracts-ethereum-package/contracts/drafts/TokenVesting.sol";
+import "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
+import "@openzeppelin/upgrades/contracts/upgradeability/ProxyAdmin.sol";
