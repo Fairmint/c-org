ce6d9c292d57ca64b97e6be58caecd17a516671c
Hardly Difficult <hardlydiff@gmail.com>
2020-07-12 18:01:45 -0700

    Simplify deps (#487)
    * Simplify dependencies
    
    * Simplify

--- contracts/Dependencies.sol
+++ contracts/Dependencies.sol
@@ -1,16 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/drafts/TokenVesting.sol";
-import "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
-import "@openzeppelin/upgrades/contracts/upgradeability/ProxyAdmin.sol";
-
 /**
  * This creates the artifacts allowing us to use these 3rd party contracts directly
  */
-contract Dependencies is TokenVesting, AdminUpgradeabilityProxy, ProxyAdmin
-{
-  constructor() internal
-    TokenVesting()
-    AdminUpgradeabilityProxy(address(0), address(0), "")
-  {}
-}
+
+import "@openzeppelin/contracts-ethereum-package/contracts/drafts/TokenVesting.sol";
+import "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
+import "@openzeppelin/upgrades/contracts/upgradeability/ProxyAdmin.sol";
